85300bb84911feb6289be1784f0b991824c37f8a
Anton Danilov <anton2009danilov@yandex.ru>
2023-01-04 17:55:26 +0300

    install rollbar-react package

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
@@ -17,37 +18,50 @@ import AuthProvider from '../contexts/AuthProvider';
 import SocketProvider from '../contexts/SocketProvider';
 import fetchInitialData from '../slices/fetchInitialData';
 import SignUp from './SignUpPage';
-// import { setLoadingStatus } from '../slices/userInterfaceSlice';
+
+const rollbarConfig = {
+  accessToken: '3eed5cc9402645968aa38d291907b42f',
+  environment: 'testenv',
+};
+
+// const TestError = () => {
+//   const a = null;
+//   return a.kello();
+// };
 
 const App = () => {
   const dispatch = useDispatch();
   const { userId: token } = window.localStorage;
 
   useEffect(() => {
-    console.log('dispatch(fetchInitialData(token))');
     dispatch(fetchInitialData(token));
   }, [dispatch, token]);
 
   return (
     <React.StrictMode>
-      <SocketProvider>
-        <AuthProvider>
-          <Router>
-            <div className="d-flex flex-column h-100">
-              <Header />
-              <Routes>
-                <Route path="/" errorElement={<ErrorPage />}>
-                  <Route index element={<MainPage />} />
-                  <Route path="login" element={<Login />} />
-                  <Route path="signup" element={<SignUp />} />
-                  <Route path="*" element={<ErrorPage />} />
-                </Route>
-              </Routes>
-              <ToastContainer />
-            </div>
-          </Router>
-        </AuthProvider>
-      </SocketProvider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <SocketProvider>
+            <AuthProvider>
+              <Router>
+                <div className="d-flex flex-column h-100">
+                  <Header />
+                  <Routes>
+                    <Route path="/" errorElement={<ErrorPage />}>
+                      <Route index element={<MainPage />} />
+                      <Route path="login" element={<Login />} />
+                      <Route path="signup" element={<SignUp />} />
+                      <Route path="*" element={<ErrorPage />} />
+                    </Route>
+                  </Routes>
+                  <ToastContainer />
+                </div>
+              </Router>
+              {/* <TestError /> */}
+            </AuthProvider>
+          </SocketProvider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </React.StrictMode>
   );
 };
